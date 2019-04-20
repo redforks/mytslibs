534c7f533ff0051244be39c36066c9ec5019e05e
Red Forks <redforks@gmail.com>
2019-04-20 22:07:54 +0800

    Import rcheck package

---
+++ packages/rcheck/src/index.ts
@@ -0,0 +1,17 @@
+export function assert(test: any, message?: string) {
+  if (!test) {
+    fail(message);
+  }
+}
+
+export function fail(message?: string): never {
+  const prefix = 'Assertion failed';
+  throw Error(message ? `${prefix}: ${message}` : prefix);
+}
+
+export function notNull<T>(v: T | null | undefined, message?: string): T {
+  if (v == null) {
+    throw Error(message || `Expect not null value, but got ${v}`);
+  }
+  return v;
+}
